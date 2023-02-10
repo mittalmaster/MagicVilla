7d01c36ae37c92f9b041286f4ae5e48100bb8b99
mittalmaster <mujjwalmittal@gmail.com>
2023-02-10 16:24:05 +0530

    Section 2 : Completed , Added basic api functionality

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,35 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //route to that controller
+    //[Route("api/[controller]")] // this will take VillaAPIController and in future if name of 
+    //file changes it will give error so thats why we are using 
+    //hardcoded VillaAPI it will not change
+    [Route("api/VillaAPI")]
+
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet] // this is the endpoints it helps to communication with other application which 
+        //is using this api 
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+
+        }
+        //[HttpGet("id")]  //this will expect id
+        [HttpGet("{id:int}")]  // in this we expilicitly define it as int               
+        public VillaDto GetVillaById(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+
+        }
+
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Dtos/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaAPI.Models.Dtos
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
