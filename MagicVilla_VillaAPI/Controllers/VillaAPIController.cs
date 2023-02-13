600ddd034efa402d2d652b3dcab533b72639b6d4
mittalmaster <mujjwalmittal@gmail.com>
2023-02-13 15:57:42 +0530

    Section 4 : Logging done ,

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -17,12 +17,21 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        private readonly ILogger<VillaAPIController> _logger;
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+            _logger = logger;
+
+        }
+
         [HttpGet] // this is the endpoints it helps to communication with other application which 
         //is using this api 
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
+            _logger.LogInformation("Getting all the Villas");
             //ActionResult is very useful when we have multiple return type 
+            
             return Ok(VillaStore.villaList);
 
         }
@@ -43,6 +52,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if(id ==0)
             {
+                _logger.LogError("Get the Error while getting with {0}" , id);
                 return BadRequest();
             }
 
