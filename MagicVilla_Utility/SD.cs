c6ae31e6bf4b2f03a200f70f9a133c2493ed7676
mittalmaster <mujjwalmittal@gmail.com>
2023-03-01 12:33:32 +0530

    Section 7 : Project setup , added consume api project ,add constant project, add models

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST, 
+            PUT, 
+            DELETE
+        }
+
+
+    }
+}
---
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static MagicVilla_Utility.SD;
+
+namespace MagicVilla_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType apiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public Object Data { get; set; }
+    }
+}
